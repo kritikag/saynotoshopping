90f53a7814b3688f9cda928c40319b73f6be40c5
Kritika <gulati.kritika@gmail.com>
2016-06-27 20:19:19 -0700

    fixed the variable names

--- script/shopblock.js
+++ script/shopblock.js
@@ -2,69 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
-//function getCurrentTabUrl(callback) {
-//    // Query filter to be passed to chrome.tabs.query - see
-//    // https://developer.chrome.com/extensions/tabs#method-query
-//    var queryInfo = {
-//        active: true,
-//        currentWindow: true
-//    };
-//    debugger;
-//    chrome.tabs.query(queryInfo, function(tabs) {
-//        // chrome.tabs.query invokes the callback with a list of tabs that match the
-//        // query. When the popup is opened, there is certainly a window and at least
-//        // one tab, so we can safely assume that |tabs| is a non-empty array.
-//        // A window can only have one active tab at a time, so the array consists of
-//        // exactly one tab.
-//        var tab = tabs[0];
-//
-//        // A tab is a plain object that provides information about the tab.
-//        // See https://developer.chrome.com/extensions/tabs#type-Tab
-//        var url = tab.url;
-//
-//        // tab.url is only available if the "activeTab" permission is declared.
-//        // If you want to see the URL of other tabs (e.g. after removing active:true
-//        // from |queryInfo|), then the "tabs" permission is required to see their
-//        // "url" properties.
-//        console.assert(typeof url == 'string', 'tab.url should be a string');
-//
-//        callback(url);
-//    });
-//
-//    // Most methods of the Chrome extension APIs are asynchronous. This means that
-//    // you CANNOT do something like this:
-//    //
-//    // var url;
-//    // chrome.tabs.query(queryInfo, function(tabs) {
-//    //   url = tabs[0].url;
-//    // });
-//    // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-//}
-//
-//
-//document.addEventListener('DOMContentLoaded', function() {
-//    getCurrentTabUrl(function(url) {
-//        // Put the image URL in Google search.
-//            console.log('after dom', url);
-//        }, function(errorMessage) {
-//            console.log('Cannot display image. ' + errorMessage);
-//        });
-//});
-var url = document.location.href;
-var shoppingArr = ['asos.com','modcloth.com','macys.com','loft.com']
+
+let url = document.location.href;
+let shoppingArr = ['asos.com','modcloth.com','macys.com','loft.com'];
+let redirectLink = "http://www.brainybudget.com/content/large/no-more-spend-budget-meme.jpg";
 let redirect;
 shoppingArr.forEach(function(shoppingUrl){
-    var x  = url.match(shoppingUrl);
-    if(x!==null){
+    var urlMatch  = url.match(shoppingUrl);
+    if(urlMatch!==null){
         redirect = true;
     }
 });
 if(redirect){
-    window.location.href = "http://www.brainybudget.com/content/large/no-more-spend-budget-meme.jpg";
+    window.location.href = redirectLink
 }
